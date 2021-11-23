f2ae2e128ad0fd3c8c82980a98c8f0e0527c14f2
EPINOT <enzo.pinot@ynov.com>
2021-11-23 16:06:29 +0100

    tetromino
    tetromino.cs

---
+++ Tetris/TetrominoCreate/TetrominoOrientation.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tetris.TetrominoCreate
+{
+    public enum TetrominoOrientation
+    {
+        UpDown,
+        LeftRight,
+        DownUp,
+        RightLeft
+    }
+}
