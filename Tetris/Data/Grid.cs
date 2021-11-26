9069c6a9c71c14daeb936745304094d0919e60a2
EPINOT <enzo.pinot@ynov.com>
2021-11-26 14:59:21 +0100

    Yasser + entrainement de class et d'héritage
    test pour voir si le cours est compris

---
+++ Tetris/Data/Grid.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace BlazorApp.Data
+{
+     public class Grid
+    {
+        public int Width { get; } = 10;
+        public int Height { get; } = 20;
+        // public CellCollection Cells { get; set; } = new CellCollection();
+
+        // public GameState State { get; set; } = GameState.NotStarted;
+
+        // public bool IsStarted
+        // {
+        //     get
+        //     {
+        //         return State == GameState.Playing 
+        //                || State == GameState.GameOver;
+        //     }
+        }
+    }
+
