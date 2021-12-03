a57463ef13623ef0c3c46baf08bb13097318d746
EPINOT <enzo.pinot@ynov.com>
2021-12-03 14:53:11 +0100

    tetris
    tetris

---
+++ Tetris/Data/Tetromino.cs
@@ -0,0 +1,78 @@
+using System;
+
+namespace BlazorApp.Data
+{
+
+   public class Tetromino
+   {
+       public int TetrominoEmplacement = 1;
+
+       public void tetromino()
+       {
+           int[,] TetrominoI = 
+            {
+                    {0,1,0,0}, 
+                    {0,1,0,0},
+                    {0,1,0,0},
+                    {0,1,0,0}
+                };
+                    
+            }
+           int[,] TetrominoO = 
+            {
+                    {0,0,0,0}, 
+                    {0,1,1,0},
+                    {0,1,1,0},
+                    {0,0,0,0}
+                };
+                              
+            
+           int[,] TetrominoT = 
+            {
+                    {0,0,0},
+                    {1,1,1},
+                    {0,1,0}
+                };
+                              
+            
+            int[,] TetrominoL = 
+            {
+                    {0,1,0},
+                    {0,1,0},
+                    {0,1,1}
+                };
+                              
+            
+
+            int[,] TetrominoJ = 
+            {
+                    {0,1,0},
+                    {0,1,0},
+                    {1,1,0}
+                };
+                              
+            
+
+            int[,] TetrominoZ = 
+            {
+                    {0,0,0},
+                    {1,1,0},
+                    {0,1,1}
+                };
+                              
+            
+
+            int[,] TetrominoS = 
+            {
+                    {0,0,0},
+                    {0,1,1},
+                    {1,1,0}
+                };
+                              
+            }
+            
+
+} 
+
+ 
+
