81e99e4aaec731348971490eb32f08bec52e372d
YasserSeryas <72004928+YasserSeryas@users.noreply.github.com>
2021-12-10 15:53:46 +0100

    UPDATE grille

--- BlazorApp/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
+++ BlazorApp/obj/Debug/net5.0/RazorDeclaration/Shared/MainLayout.razor.g.cs
@@ -13,70 +13,70 @@ namespace BlazorApp.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "/Users/enzo/Downloads/TetrisProject-Yasser/BlazorApp/_Imports.razor"
+#line 1 "C:\Users\Serya\OneDrive\Documents\GitHub\TetrisProject\BlazorApp\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "/Users/enzo/Downloads/TetrisProject-Yasser/BlazorApp/_Imports.razor"
+#line 2 "C:\Users\Serya\OneDrive\Documents\GitHub\TetrisProject\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "/Users/enzo/Downloads/TetrisProject-Yasser/BlazorApp/_Imports.razor"
+#line 3 "C:\Users\Serya\OneDrive\Documents\GitHub\TetrisProject\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "/Users/enzo/Downloads/TetrisProject-Yasser/BlazorApp/_Imports.razor"
+#line 4 "C:\Users\Serya\OneDrive\Documents\GitHub\TetrisProject\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "/Users/enzo/Downloads/TetrisProject-Yasser/BlazorApp/_Imports.razor"
+#line 5 "C:\Users\Serya\OneDrive\Documents\GitHub\TetrisProject\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "/Users/enzo/Downloads/TetrisProject-Yasser/BlazorApp/_Imports.razor"
+#line 6 "C:\Users\Serya\OneDrive\Documents\GitHub\TetrisProject\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "/Users/enzo/Downloads/TetrisProject-Yasser/BlazorApp/_Imports.razor"
+#line 7 "C:\Users\Serya\OneDrive\Documents\GitHub\TetrisProject\BlazorApp\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web.Virtualization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "/Users/enzo/Downloads/TetrisProject-Yasser/BlazorApp/_Imports.razor"
+#line 8 "C:\Users\Serya\OneDrive\Documents\GitHub\TetrisProject\BlazorApp\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "/Users/enzo/Downloads/TetrisProject-Yasser/BlazorApp/_Imports.razor"
+#line 9 "C:\Users\Serya\OneDrive\Documents\GitHub\TetrisProject\BlazorApp\_Imports.razor"
 using BlazorApp;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 10 "/Users/enzo/Downloads/TetrisProject-Yasser/BlazorApp/_Imports.razor"
+#line 10 "C:\Users\Serya\OneDrive\Documents\GitHub\TetrisProject\BlazorApp\_Imports.razor"
 using BlazorApp.Shared;
 
 #line default
