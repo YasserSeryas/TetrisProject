996803b3c6893d59bf56fdfa8f46212aacd4c351
Karrwolf <maloloyerviaud@gmail.com>
2021-12-10 10:17:09 +0100

    correction downpos

--- DownPos.cs
+++ DownPos.cs
@@ -1,31 +1,19 @@
 public event EventHandler Tick;
-    {
-        downPos++;
-        moveShape();
-        if (gameSpeed => slow)
-        {
-            gameSpeed += (gameSlow/500f);
-        }
-        elseif (gameSpeed => normal){
-            gameSpeed += (gameNormal/1000f);
-        }
-        elseif (gameSpeed => fast){
-            gameSpeed += (gameFast/2000f);
-        }
-        else(keyBinding){
-            case "Down":
-                downPos++;
-        }
-
-////////////////////////////////////////
-        switch (downPos++)
 {
-    case  constante :
-         instructions;
-         break;
-
-   [default :
-        instructions;
-        break;]
-}
+    downPos++;
+    moveShape();
+    if (gameSpeed => slow)
+    {
+        gameSpeed += (gameSlow/500f);
     }
+    elseif (gameSpeed => normal){
+        gameSpeed += (gameNormal/1000f);
+    }
+    elseif (gameSpeed => fast){
+        gameSpeed += (gameFast/2000f);
+    }
+    else(keyBinding){
+        case "Down";
+        downPos++;
+    }
+}
